4e129f324643bd07c344f27ca59177c45ab4942c
janeffer945 <janeffernjeri945@gmail.com>
2022-06-13 11:13:41 +0300

    created class profile modes

--- ratings/models.py
+++ ratings/models.py
@@ -7,3 +7,8 @@ from django.db import models
 
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = CloudinaryField('image')
+    bio = models.CharField(max_length=500, blank=True, null=True)
+    contact = models.CharField(max_length=50, blank=True, null=True)
