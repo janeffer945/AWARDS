ff9f61ed5e25ba9ece45ab819cb7f33eade1316a
janeffer945 <janeffernjeri945@gmail.com>
2022-06-13 10:52:08 +0300

    imported cloudinary

--- ratings/models.py
+++ ratings/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+
+from django.db import models
+
+
 
 # Create your models here.
