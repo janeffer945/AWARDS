b78aa7ff6a77f1f7cc05f00f3714ec40c1b8587b
janeffer945 <janeffernjeri945@gmail.com>
2022-06-13 10:45:40 +0300

    created profilelist apiview

--- ratings/views.py
+++ ratings/views.py
@@ -43,23 +43,6 @@ def upload_project(request):
     return render(request,"all-awards/upload_project.html",{'form':form})
 
 
-    #Create user Proffile
-def create_profile(request):
-    current_user = request.user
-    title = "Create Profile"
-    if request.method == 'POST':
-        form = CreateProfileForm(request.POST, request.FILES)
-        if form.is_valid():
-            profile = form.save(commit=False)
-            profile.user = current_user
-            profile.save()
-        return HttpResponseRedirect('/')
-
-    else:
-        form = CreateProfileForm()
-    return render(request, 'all-awards/create_profile.html', {"form": form, "title": title})
-
-
     #Update USer Profile
 def update_profile(request,id):
     user = User.objects.get(id=id)
@@ -81,3 +64,52 @@ def project_details(request, project_id):
     rating = Rating.objects.filter(project = project)
     # get project rating
     return render(request, "all-awards/project_details.html", {"project": project, "rating": rating})
+
+# rate_project
+@login_required(login_url='/accounts/login/')
+def rate(request,id):
+    if request.method == 'POST':
+        project = Project.objects.get(id = id)
+        current_user = request.user
+        design_rate = request.POST['design']
+        content_rate = request.POST['content']
+        usability_rate = request.POST['usability']
+
+        Rating.objects.create(
+            project=project,
+            user=current_user,
+            design_rate=design_rate,
+            usability_rate=usability_rate,
+            content_rate=content_rate,
+            avg_rate=round((float(design_rate)+float(usability_rate)+float(content_rate))/3,2),)
+
+        return render(request,"all-awards/project_details.html",{"project":project})
+    else:
+        project = Project.objects.get(id = id) 
+        return render(request,"all-awards/project_details.html",{"project":project})
+    
+@login_required(login_url='/accounts/login/')
+def search_project(request):
+    if 'search' in request.GET and request.GET['search']:
+        search_term = request.GET.get('search').lower()
+        projects = Project.search_project_name(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'all-awards/search.html', {'found': message, 'projects': projects})
+    else:
+        message = 'Not found'
+        return render(request, 'all-awards/search.html', {'danger': message})
+    
+class ProjectList(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get(self,request,format=None):
+        projects = Project.objects.all()
+        serializer = ProjectSerializer(projects,many=True)
+        return Response(serializer.data)
+
+class ProfileList(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get(self,request,format=None):
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles,many=True)
+        return Response(serializer.data)
